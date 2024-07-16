fc3275601bf26afa88e49ab7f70156a2a15b4462
DennisMuhami <ndamburidennis2006@gmail.com>
2024-07-16 15:46:05 +0300

    fix login route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/register';
 import Login from './pages/login';
-import Home from './pages/Home';
 
 const App = () => {
   return (
@@ -11,7 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </Router>
   );
